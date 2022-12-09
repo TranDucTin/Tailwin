56f842312ea0c04bf89dfd9bfae3cc48102b7086
TranDucTin <106247444+TranDucTin@users.noreply.github.com>
2022-12-09 22:19:57 +0700

    CSS vs HTML slick slider

---
+++ module-slick_slider.blade.php
@@ -0,0 +1,49 @@
+<section class="slickdemo relative" >
+  <div class="">
+    <div class="autoplay slickdemo-posi">
+      <div class="bg-white">
+        <div class="slick-item">
+          <h1>
+            “We've done more with Encompass in the past 6 months than we did in 3 years on our previous solution.”
+          </h1>
+          <p>David Little
+            CTO, Savannah Distributing</p>
+        </div>
+      </div>
+      <div class="bg-white">
+        <div class="slick-item">
+          <h1>
+            “We've done more with Encompass in the past 6 months than we did in 3 years on our previous solution.”
+          </h1>
+          <p>David Little
+            CTO, Savannah Distributing</p>
+        </div>
+      </div>
+      <div class="bg-white">
+        <div class="slick-item">
+          <h1>
+            “We've done more with Encompass in the past 6 months than we did in 3 years on our previous solution.”
+          </h1>
+          <p>David Little
+            CTO, Savannah Distributing</p>
+        </div>
+      </div>
+      <div class="bg-white">
+        <div class="slick-item">
+          <h1>
+            “We've done more with Encompass in the past 6 months than we did in 3 years on our previous solution.”
+          </h1>
+          <p>David Little
+            CTO, Savannah Distributing</p>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="flex absolute slick-button right-0 -mr-19 bottom-30">
+    <button type="button" class="bg-orange-400 slick-left">
+      <span class="text-4xl text-white icomoon icon-chevron-left"></span>
+    <button type="button" class="bg-orange-400 slick-right">
+      <span class="text-4xl text-white icomoon icon-chevron-right"></span>
+    </button>
+  </div>
+</section>
